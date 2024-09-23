542899e4d1204c0cc8cfa028ab4f79a8a6616f45
nishanthn99 <21e32.nishanth@sjec.ac.in>
2024-09-23 22:51:26 +0530

    level2

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -5,32 +5,53 @@ interface TaskCard {
   assigneeName: string
 }
 
-class Card implements TaskCard {
+class dueCard implements TaskCard {
   title: string
   assigneeName: string
   dueDate: string
-  completedAtDate:string
 
-  constructor(title:string, assigneeName:string, dueDate: string,completedAtDate:string){
+  constructor(title:string, assigneeName:string, dueDate: string){
       this.title = title
       this.assigneeName = assigneeName
       this.dueDate = dueDate
+  }
+}
+
+class completedCard implements TaskCard {
+  title: string
+  assigneeName: string
+  completedAtDate: string
+
+  constructor(title:string, assigneeName:string, completedAtDate: string){
+      this.title = title
+      this.assigneeName = assigneeName
       this.completedAtDate = completedAtDate
   }
 }
 
 
 const TaskCard = ({title,dueDate,assigneeName,completedAtDate}) => {
-  const pending=new Card(title,assigneeName,dueDate,completedAtDate);
-  return (
-    <div className='task-card p-4'>
-      <h2 className="text-xl font-bold">{pending.title}</h2>
-      <p>{pending.dueDate}</p>
-      <p>{pending.completedAtDate}</p>
-      <p>Assignee:{pending.assigneeName}</p>
-
-    </div>
-  )
+  if (dueDate != null){
+    const item = new dueCard(title, assigneeName,dueDate)
+    return (
+            <div className='task-card mt-3 m-3'>
+                <h1 className='font-bold text-2xl'>{item.title}</h1>
+                <p>Due on: {item.dueDate}</p>
+                <p>Assignee: {item.assigneeName}</p>
+            </div>
+    )
+} else{ 
+    const item = new completedCard(title,assigneeName,completedAtDate)
+    return (
+        <>
+            <div className='task-card mt-3 m-3'>
+                <h1 className='font-bold text-2xl'>{item.title}</h1>
+                <p>Completed on: {item.completedAtDate}</p>
+                <p>Assignee: {item.assigneeName}</p>
+            </div>
+        </>
+    )
+}
 }
 
 export default TaskCard
